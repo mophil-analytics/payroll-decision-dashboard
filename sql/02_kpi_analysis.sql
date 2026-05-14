e468ea23d0a4bed58b6bd8c48706a5e237dfb19d
Philip Ojo <mophilwriter@gmail.com>
2026-05-14 13:41:34 +0100

    Create 02_kpi_analysis.sql

---
+++ sql/02_kpi_analysis.sql
@@ -0,0 +1,9 @@
+## KPI & BUSINESS ANALYSIS
+- Total payroll cost
+- Total workforce cost
+- Department payroll ranking
+- Overtime exposure
+- Bonus analysis
+- Payroll variance
+- Compensation ranking
+- Executive KPI queries
